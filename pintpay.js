761b90223bb2bd0274f301bd174ae7009a9931fa
Philip Woods <painteroflight@gmail.com>
2011-12-05 16:57:28 +0000

    cleaned up the code a bit

--- pintpay.js
+++ pintpay.js
@@ -4,38 +4,39 @@ var PintPay = exports.PintPay = function(key, secret) {
     this.api_key    = key;
     this.api_secret = secret;
     this.PINTPAY_BASE_URL = "https://www.pintpay.com/api/1/";
-}
+};
 
 PintPay.prototype.dispatchRequest = function(request, callback, shouldPut){
-    requestURL = this.PINTPAY_BASE_URL + request + "?api_key=" + this.api_key + "&api_secret=" + this.api_secret;
-    if(shouldPut == null){
+    var requestURL = this.PINTPAY_BASE_URL + request + "?api_key=" + this.api_key + "&api_secret=" + this.api_secret;
+    if(shouldPut === null){
         rest.get(requestURL).on('complete', function(data) {
             callback(data);
         }).on('error', function(error) {
             console.log("PINTPAY API ERROR :: " + error);
-        });;
-    }else if(shouldPut == true){
+        });
+    }else if(shouldPut === true){
         rest.put(requestURL).on('complete', function(data) {
             callback(data);
         }).on('error', function(error) {
             console.log("PINTPAY API ERROR :: " + error);
-        });;
+        });
     }
     
-}
+};
 
 PintPay.prototype.getAllSubscriptions = function(callback){
     return this.dispatchRequest("subscriptions", callback);
-}
+};
 
 PintPay.prototype.getSubscription = function(secret, callback){
     return this.dispatchRequest("subscriptions/" + secret, callback);
-}
+};
 
 PintPay.prototype.cancelSubscription = function(secret, callback){
-    if(secret != null && (typeof secret == "string") && secret != "")
+    if(secret !== null && (typeof secret == "string") && secret != ""){
         return this.dispatchRequest("subscriptions/" + secret + "/cancel", callback, true);
-    else
+    }else{
         console.log("you can not cancel a subscription without providing a secret");
         return null;
-}
+    }
+};
